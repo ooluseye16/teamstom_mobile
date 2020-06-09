619aaf888266b7541b18b0798bbdb9ff68b9adf6
osebi-dev <adams.osebi@gmail.com>
2020-06-09 17:18:17 +0100

    chore: BrainTease app splash screen done

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,46 @@
-import "package:flutter/material.dart";
-import 'screens/homepage.dart';
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+import 'dart:async';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MaterialApp(home: MyApp()));
+}
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    // Initialize a 3 seconds delay
+    Future.delayed(
+      Duration(seconds: 5),
+      () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => HomePage(),
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomePage(),
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          new Container(
+            decoration: BoxDecoration(
+                image: new DecorationImage(
+                    image: AssetImage('assets/splash.jpg'), fit: BoxFit.fill)),
+          ),
+        ],
+      ),
     );
   }
 }
