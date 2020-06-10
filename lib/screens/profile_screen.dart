1f7a07e52b4c5b1bf88e57d7cee669a859a2b7be
Oluseye Obitola <oluseyeobitola1@gmail.com>
2020-06-10 17:26:15 +0100

    Add files via upload

---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:teamstommobile/constants.dart';
+import 'package:teamstommobile/screens/diffculty.dart';
+
+class ProfileScreen extends StatefulWidget {
+
+
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+
+  String username = '';
+  Future<String> getStringValuesSF() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String stringValue = prefs.getString('USERNAME');
+    return stringValue;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: primaryColor,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+              child: Container(
+                padding: EdgeInsets.only(top: 30.0, left: 20.0, right: 20.0),
+            color: Colors.white,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                FutureBuilder<String>(
+                future: getStringValuesSF(),
+                  initialData: 'Guest',
+                  builder: (context, snapshot){
+                  username = snapshot.data;
+                  return snapshot.hasData? Text("Hi $username", style: TextStyle(color: deepOrange, fontSize: 24.0, fontWeight: FontWeight.bold),) : Text("Hi Guest");
+                  },
+            ),
+                FutureBuilder(
+                  future: getStringValuesSF(),
+                  builder:(context, snapshot) => CircleAvatar(
+                    backgroundColor: primaryColor,
+                    radius: 40.0,
+                    child: Text(snapshot.data[0], style: TextStyle(color: deepOrange, fontSize: 24.0, fontWeight: FontWeight.bold)),
+                  ),
+                ),
+              ],
+            ),
+          )),
+          Expanded(
+              flex: 4,
+              child: Container(
+                padding: EdgeInsets.only(bottom: 30.0),
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: ButtonTheme(
+                    height: 50.0,
+                    minWidth: 150.0,
+                    child: RaisedButton(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0)
+                      ),
+                      onPressed: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context)=>DifficultySelectionPage()));
+                      },
+                      child: Text("START GAME", style: TextStyle(color: deepOrange, fontWeight: FontWeight.bold, fontSize: 18.0),),
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              )),
+        ],
+      ),
+    );
+  }
+}
