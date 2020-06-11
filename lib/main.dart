1410f8dafff979dfcba696fdd60201feed03dab3
Festus <festusowumi@gmail.com>
2020-06-11 01:29:08 +0100

    fixed device orientation error

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:teamstommobile/screens/splash_screen.dart';
 import 'package:teamstommobile/constants.dart';
 
 
 void main() {
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp,DeviceOrientation.portraitDown]);
   runApp(MaterialApp(
     title: "BrainTease",
     color: primaryColor ,
